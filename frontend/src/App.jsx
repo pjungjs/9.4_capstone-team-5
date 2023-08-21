2114d216e748ea5769bfa5073c5bdd38b76d346a
pjungjs <pjungjs@gmail.com>
2023-08-21 19:12:18 -0400

    fe_initial setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,27 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import NavBar from './components/common/NavBar.jsx';
+import Footer from './components/common/Footer.jsx';
+import Home from './pages/Home.jsx';
+import About from './pages/About.jsx';
+import NotFound from './pages/NotFound.jsx';
+
 function App() {
   return (
-    <div className="flex items-center justify-center h-screen font-bold">
-      Hello World
+    <div className="App">
+      <Router>
+        <header>
+          <NavBar />
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </main>
+        <Footer />
+      </Router>
     </div>
   );
 }
