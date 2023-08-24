3bc30560a8246d879e4b4dde518da030831c3e9d
pjungjs <pjungjs@gmail.com>
2023-08-24 02:44:44 -0400

    fe_updated Contact

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,26 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import ScrollTop from './utils/scrollTop.js';
 import NavBar from './components/common/NavBar.jsx';
 import Footer from './components/common/Footer.jsx';
 import Home from './pages/Home.jsx';
 import About from './pages/About.jsx';
+import Contact from './pages/Contact.jsx';
 import NotFound from './pages/NotFound.jsx';
-import ScrollToTop from './utils/ScrollToTop.js';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <ScrollToTop />
+        <ScrollTop />
         <header className="sticky top-0 w-full">
           <NavBar />
         </header>
-        <main className="flex h-screen justify-center">
+        <main>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </main>
