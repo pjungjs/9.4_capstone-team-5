5c47b32e922bd6364db352c0133ce71bb678e09c
Wilsantos1975 <Faustosantos@pursuit.org>
2023-08-26 23:52:56 -0400

    added testimonial section and worked on the images sizing

---
+++ frontend/src/components/LandingComponents/Testimonial.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+
+function Testimonial() {
+  return (
+    
+<div className="grid mb-8 border border-gray-200 rounded-lg shadow-sm dark:border-gray-700 md:mb-12 md:grid-cols-2 ">
+    <figure className="flex flex-col items-center justify-center p-8 text-center bg-white border-b border-gray-200 rounded-t-lg md:rounded-t-none md:rounded-tl-lg md:border-r dark:bg-gray-800 dark:border-gray-700">
+        <blockquote className="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
+            <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Very easy this was to integrate</h3>
+            <p className="my-4">As an orchestra director, I'm accustomed to harmonizing melodies, but Ecoway harmonizes sustainability into my daily routine. This app has transformed the way I approach eco-friendly practices. Its user-friendly interface and informative resources make it easy to integrate sustainable choices into my lifestyle. Ecoway has truly struck the right chord in promoting a greener world.</p>
+        </blockquote>
+        <figcaption className="flex items-center justify-center space-x-3">
+            <img className="rounded-full w-9 h-9" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/karen-nelson.png" alt="profile picture"/>
+            <div className="space-y-0.5 font-medium dark:text-white text-left">
+                <div>Tom Devoe</div>
+                <div className="text-sm text-gray-500 dark:text-gray-400">Orchestra Director</div>
+            </div>
+        </figcaption>    
+    </figure>
+    <figure className="flex flex-col items-center justify-center p-8 text-center bg-white border-b border-gray-200 rounded-tr-lg dark:bg-gray-800 dark:border-gray-700">
+        <blockquote className="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
+            <h3 className="text-lg font-semibold text-gray-900 dark:text-white">it's a bridge that connects our actions to a greener future.</h3>
+            <p className="my-4">Running a successful business while nurturing the environment can be challenging. Ecoway, however, has revolutionized our approach at the R.O.C. Its comprehensive solutions for eco-conscious practices have not only reduced our carbon footprint but also elevated our commitment to sustainability. With Ecoway, we're pioneering a greener future for our company and the planet.</p>
+        </blockquote>
+        <figcaption className="flex items-center justify-center space-x-3">
+            <img className="rounded-full w-9 h-9" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/roberta-casas.png" alt="profile picture"/>
+            <div className="space-y-0.5 font-medium dark:text-white text-left">
+                <div>Pastora Lara Portillo</div>
+                <div className="text-sm text-gray-500 dark:text-gray-400">CEO of the R.O.C.</div>
+            </div>
+        </figcaption>    
+    </figure>
+    <figure className="flex flex-col items-center justify-center p-8 text-center bg-white border-b border-gray-200 rounded-bl-lg md:border-b-0 md:border-r dark:bg-gray-800 dark:border-gray-700">
+        <blockquote className="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
+            <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Ecoway empowers individuals from all walks of life to be agents of positive change.</h3>
+            <p className="my-4">Entrepreneurship is about innovation and impact, and Ecoway embodies both. This app has opened my eyes to eco-friendly avenues that align with my business goals. From reducing waste to supporting green initiatives, Ecoway provides actionable steps that empower entrepreneurs like me to contribute positively to the environment while driving business success.</p>
+        </blockquote>
+        <figcaption className="flex items-center justify-center space-x-3">
+            <img className="rounded-full w-9 h-9" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png" alt="profile picture"/>
+            <div className="space-y-0.5 font-medium dark:text-white text-left">
+                <div>Steve Biko</div>
+                <div className="text-sm text-gray-500 dark:text-gray-400">Entreprenour</div>
+            </div>
+        </figcaption>    
+    </figure>
+    <figure className="flex flex-col items-center justify-center p-8 text-center bg-white border-gray-200 rounded-b-lg md:rounded-br-lg dark:bg-gray-800 dark:border-gray-700">
+        <blockquote className="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
+            <h3 className="text-lg font-semibold text-gray-900 dark:text-white">It's about making sustainable choices </h3>
+            <p className="my-4">Ecoway's data-driven insights and tools have enhanced my understanding of environmentally conscious investments. Ecoway empowers me to make informed decisions that align with my ethical values and financial acumen.
+
+</p>
+        </blockquote>
+        <figcaption className="flex items-center justify-center space-x-3">
+            <img className="rounded-full w-9 h-9" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/joseph-mcfall.png" alt="profile picture"/>
+            <div className="space-y-0.5 font-medium dark:text-white text-left">
+                <div>William Cashman</div>
+                <div className="text-sm text-gray-500 dark:text-gray-400">Stock Analyst</div>
+            </div>
+        </figcaption>    
+    </figure>
+</div>
+
+
+  )
+}
+
+export default Testimonial
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 
 import AboutSection from '../components/LandingComponents/AboutSection';
 import LandingMain from '../components/LandingComponents/LandingMain';
+import Testimonial from '../components/LandingComponents/Testimonial';
 
 function Home() {
   return (
-    <div className="main-landing flex flex-col">
+    <div className="shrink w-65 h-14">
       <LandingMain />
       <AboutSection />
+      <Testimonial/>
     </div>
   );
 }
