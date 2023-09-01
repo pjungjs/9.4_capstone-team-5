ce486dd8ea0fd5bcfc822e56e790e664542ec443
pjungjs <pjungjs@gmail.com>
2023-08-31 23:07:06 -0400

    fe_updated Stytch client. switched from UI client to Headless client

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { StytchProvider } from '@stytch/react';
-import { StytchUIClient } from '@stytch/vanilla-js';
+import { StytchHeadlessClient } from '@stytch/vanilla-js/headless';
 import App from './App.jsx';
 import './index.css';
 
-const stytch = new StytchUIClient(import.meta.env.VITE_AUTH_PUBLIC_TOKEN);
+const stytch = new StytchHeadlessClient(import.meta.env.VITE_AUTH_PUBLIC_TOKEN);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
