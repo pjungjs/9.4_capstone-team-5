6cb0d4672d5a27d9e7adeb5de645cb692f578836
Wilsantos1975 <Faustosantos@pursuit.org>
2023-09-01 21:48:00 -0400

    added assets file for badges images

--- backend/db/seed.sql
+++ backend/db/seed.sql
@@ -1,4 +1,6 @@
 \c badges;
 
 INSERT INTO badges (badge_name, badge_description, badge_image, badge_points) VALUES 
-('First Badge', 'You got your first badge!', 'https://i.imgur.com/1QgrNNw.png', 10);
+('First Badge', 'You got your first badge!', 'backend/db/assets/1.svg', 10);
+('second badge', 'this is a second badge', 'backend/db/assets/2.svg',11);
+('Third Badge', )
