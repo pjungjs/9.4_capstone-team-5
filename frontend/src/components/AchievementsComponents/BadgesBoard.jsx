8d48ea38f9466b458baf6abb46aa2c24d5eacfd3
Wilsantos1975 <Faustosantos@pursuit.org>
2023-09-04 13:03:57 -0400

    badges displaying, working on rendering images from the backend

--- frontend/src/components/AchievementsComponents/BadgesBoard.jsx
+++ frontend/src/components/AchievementsComponents/BadgesBoard.jsx
@@ -29,19 +29,19 @@ useEffect(() => {
       <div className="flex flex-wrap items-center justify-center py-4 md:py-8">
         <button
           type="button"
-          className="mb-3 mr-3 rounded-full border border-blue-600 bg-white px-5 py-2.5 text-center text-base font-medium text-blue-700 hover:bg-blue-700 hover:text-white focus:outline-none focus:ring-4 focus:ring-blue-300 dark:border-blue-500 dark:bg-gray-900 dark:text-blue-500 dark:hover:bg-blue-500 dark:hover:text-white dark:focus:ring-blue-800"
+          className="mb-3 mr-3 rounded-full border border-blue-600 bg-white px-5 py-2.5 text-center text-base font-medium text-blue-700 hover:bg-blue-700 hover:text-white focus:outline-none focus:ring-4 focus:ring-blue-300"
         >
           All Badges
         </button>
         <button
           type="button"
-          className="mb-3 mr-3 rounded-full border border-white bg-white px-5 py-2.5 text-center text-base font-medium text-gray-900 hover:border-gray-200 focus:outline-none focus:ring-4 focus:ring-gray-300 dark:border-gray-900 dark:bg-gray-900 dark:text-white dark:hover:border-gray-700 dark:focus:ring-gray-800"
+          className="mb-3 mr-3 rounded-full border border-white bg-white px-5 py-2.5 text-center text-base font-medium text-gray-900 hover:border-gray-200 focus:outline-none focus:ring-4 focus:ring-gray-300"
         >
           Achieved
         </button>
         <button
           type="button"
-          className="mb-3 mr-3 rounded-full border border-white bg-white px-5 py-2.5 text-center text-base font-medium text-gray-900 hover:border-gray-200 focus:outline-none focus:ring-4 focus:ring-gray-300 dark:border-gray-900 dark:bg-gray-900 dark:text-white dark:hover:border-gray-700 dark:focus:ring-gray-800"
+          className="mb-3 mr-3 rounded-full border border-white bg-white px-5 py-2.5 text-center text-base font-medium text-gray-900 hover:border-gray-200 focus:outline-none focus:ring-4 focus:ring-gray-300"
         >
           Bags
         </button>
@@ -54,11 +54,11 @@ useEffect(() => {
     </div> */}
 
 
-<div className=' items-center grid grid-cols-3 gap-4 '>
+
   {badge.map((badge) => (
-    <Badge badge={badge} />
+    <Badge key={badge.badge_id} badge={badge} />
   ))}
-</div>
+
        
       </div>
     </div>
