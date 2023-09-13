f49845fe5c67a6eb51e8b214a5c112fe285f72b3
Wilsantos1975 <Faustosantos@pursuit.org>
2023-09-12 20:48:25 -0400

    css added to the badges data populated

--- backend/db/seed.sql
+++ backend/db/seed.sql
@@ -27,13 +27,20 @@ INSERT INTO badges
   (badge_name, badge_description, image, badge_points)
 VALUES 
   ('Recycle Hero', 'You got your first badge!', 'https://ecoway.s3.amazonaws.com/LOGO.svg', 10),
-  ('Next Level', 'this is a second badge','https://images.vexels.com/media/users/3/245747/isolated/preview/fc5e5179e126bb8b8878c65ed0639179-great-job-badge.png', 11),
+  ('Water Wizard', 'You have reduced your water consumption by 20%. Splish, splash, you are making a splash!','https://images.vexels.com/media/users/3/245747/isolated/preview/fc5e5179e126bb8b8878c65ed0639179-great-job-badge.png', 20),
   ('Feedback contributor', 'this is the third badge', 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/Seal_of_the_President_of_the_United_States.svg/2424px-Seal_of_the_President_of_the_United_States.svg.png', 4),
-  ('Energy Saver', 'this is the third badge', 'https://icon2.cleanpng.com/20180320/qoe/kisspng-computer-icons-iconfinder-award-top-seller-icon-png-5ab0932f750863.9298772115215214554794.jpg', 134),
+  ('Carbon Footprint Fairy', 'You have calculated and reduced your carbon footprint by 25%. You are leaving smaller footsteps on Earth!', 'https://icon2.cleanpng.com/20180320/qoe/kisspng-computer-icons-iconfinder-award-top-seller-icon-png-5ab0932f750863.9298772115215214554794.jpg', 50),
   ('Reusable Crusader', 'this is the third badge', 'backend/db/assets/6.png', 9726),
   ('Consistent Player', 'this is the hsii badge', 'backend/db/assets/6.png', 134),
   ('Everything local', 'this is the third badge', 'backend/db/assets/7.png', 134),
-  ('8 Badge', 'this is the third badge', 'backend/db/assets/8.png', 87),
-  ('9 Badge', 'this is the blah blah badge', 'backend/db/assets/9.png', 134),
-  ('X Badge', 'this is the third badge', 'backend/db/assets/10.png', 9726),
-  ('11 Badge', 'this is the third badge', 'backend/db/assets/11.png', 9726);
+  ('Trash Tamer', 'Youve gone zero-waste for a month. Your trash can is on vacation!', 'backend/db/assets/8.png', 87),
+  ('Bicycle Boss', 'You have biked 100 miles instead of driving. Your calves thank you, and so does the planet!', 'backend/db/assets/9.png', 20),
+  ('Ocean Saver', 'You have picked up 100 pieces of trash at the beach. You are turning the tide on pollution!', 'backend/db/assets/10.png', 9726),
+  ('Green Gremlin Guru', 'You have reduced energy consumption by 30%. You are the master of energy conservation!', 'backend/db/assets/11.png', 9726),
+  ('Green Gremlin Guru', 'You have reduced energy consumption by 30%. You are the master of energy conservation!', 'backend/db/assets/11.png', 9726),
+  ('Green Gremlin Guru', 'You have reduced energy consumption by 30%. You are the master of energy conservation!', 'backend/db/assets/11.png', 9726),
+  ('Green Gremlin Guru', 'You have reduced energy consumption by 30%. You are the master of energy conservation!', 'backend/db/assets/11.png', 9726),
+  ('Green Gremlin Guru', 'You have reduced energy consumption by 30%. You are the master of energy conservation!', 'backend/db/assets/11.png', 9726),
+  ('Green Gremlin Guru', 'You have reduced energy consumption by 30%. You are the master of energy conservation!', 'backend/db/assets/11.png', 9726),
+  ('Green Gremlin Guru', 'You have reduced energy consumption by 30%. You are the master of energy conservation!', 'backend/db/assets/11.png', 9726),
+  ('Green Gremlin Guru', 'You have reduced energy consumption by 30%. You are the master of energy conservation!', 'backend/db/assets/11.png', 9726);
--- frontend/src/components/AchievementsComponents/BadgesBoard.jsx
+++ frontend/src/components/AchievementsComponents/BadgesBoard.jsx
@@ -4,28 +4,25 @@ import axios from 'axios';
 import { useState, useEffect } from 'react';
 import badge1 from '../../../../backend/db/assets/1.png';
 
-
-const API = import.meta.env.VITE_BASE_URL
+const API = import.meta.env.VITE_BASE_URL;
 
 // const badgeImage = '../../../../'
 
-
 function BadgesBoard() {
+  const [badge, setBadge] = useState([]);
 
-const [badge, setBadge] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`${API}/badges`)
 
-useEffect(() => {
-    axios.get(`${API}/badges`)
-    
-        .then((res) => {
-            console.log(res.data);
-            setBadge(res.data);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-}, []);
-
+      .then((res) => {
+        console.log(res.data);
+        setBadge(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
   return (
     <div>
@@ -42,28 +39,19 @@ useEffect(() => {
         >
           Achieved
         </button>
-        <button
-          type="button"
-          className="mb-3 mr-3 rounded-full border border-white bg-white px-5 py-2.5 text-center text-base font-medium text-gray-900 hover:border-gray-200 focus:outline-none focus:ring-4 focus:ring-gray-300"
-        >
-          Bags
-        </button>
+
         {/* <button type="button" className="text-gray-900 border border-white hover:border-gray-200 dark:border-gray-900 dark:bg-gray-900 dark:hover:border-gray-700 bg-white focus:ring-4 focus:outline-none focus:ring-gray-300 rounded-full text-base font-medium px-5 py-2.5 text-center mr-3 mb-3 dark:text-white dark:focus:ring-gray-800">Electronics</button> */}
         {/* <button type="button" className="text-gray-900 border border-white hover:border-gray-200 dark:border-gray-900 dark:bg-gray-900 dark:hover:border-gray-700 bg-white focus:ring-4 focus:outline-none focus:ring-gray-300 rounded-full text-base font-medium px-5 py-2.5 text-center mr-3 mb-3 dark:text-white dark:focus:ring-gray-800">Gaming</button> */}
       </div>
-      <div className="grid grid-cols-2 gap-4 md:grid-cols-3">
+      <div className="grid grid-cols-2 gap-4  p-12 md:grid-cols-3 ">
         {/* <div>
         <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg" alt=""/>
     </div> */}
 
-
-
-  {badge.map((badge) => (
-    <Badge key={badge.badge_id} badge={badge} />
-    // <img src={`${badgeImage}${badge.image} `} key={badge.badge_id}/>
-  ))}
-
-       
+        {badge.map((badge) => (
+          <Badge key={badge.badge_id} badge={badge} />
+          // <img src={`${badgeImage}${badge.image} `} key={badge.badge_id}/>
+        ))}
       </div>
     </div>
   );
