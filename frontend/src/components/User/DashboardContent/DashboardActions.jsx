559fbad367183cfc446771b50da15d127e30c137
pjungjs <pjungjs@gmail.com>
2023-09-17 01:44:28 -0400

    fe_added basic structures and contents on the Dashboard

---
+++ frontend/src/components/User/DashboardContent/DashboardActions.jsx
@@ -0,0 +1,9 @@
+function DashboardActions() {
+  return (
+    <div className="rounded-lg border border-gray-300 p-4 shadow-md">
+      Here goes the Actions that you can do today to contribute for greener planet!
+    </div>
+  );
+}
+
+export default DashboardActions;
--- frontend/src/components/User/DashboardContent/DashboardWelcome.jsx
+++ frontend/src/components/User/DashboardContent/DashboardWelcome.jsx
@@ -53,7 +53,7 @@ function DashboardWelcome() {
   ];
 
   return (
-    <div className="rounded-b-lg">
+    <div className="rounded-b-lg bg-gradient-to-r from-green-500 to-teal-500">
       <div className="w-full p-5 text-center text-xl font-bold text-gray-800">
         {welcomeMessages[Math.floor(Math.random() * welcomeMessages.length)]}
       </div>
