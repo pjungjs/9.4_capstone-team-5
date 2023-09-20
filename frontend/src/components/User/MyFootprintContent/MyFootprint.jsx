0c8a8dedd1bf4c62b33806ca9c2be9e3286ee55c
JoseC620 <Jcepeda6201@gmail.com>
2023-09-20 18:42:02 -0400

    some footprint page stuff

--- frontend/src/components/User/MyFootprintContent/MyFootprint.jsx
+++ frontend/src/components/User/MyFootprintContent/MyFootprint.jsx
@@ -1,5 +1,11 @@
-import { useState, useEffect, useContext } from "react"
-import axios from "axios"
+import { useState, useEffect } from "react";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Pie } from "react-chartjs-2";
+import axios from "axios";
+
+ChartJS.register( ArcElement, Tooltip, Legend );
+
+
 
 const API = import.meta.env.VITE_BASE_URL
 
@@ -19,9 +25,47 @@ export default function MyFootprint() {
             });
     }, []);
 
+
+    const data = {
+        labels: ['One', 'Two', 'Three'],
+        datasets: [
+            {
+                data: [3, 6, 9],
+                backgroundColor: ['aqua', 'yellow', 'purple']
+            }
+            
+        ]
+    }
+
+    const options = {
+        responsive: true,
+        plugins: {
+          legend: {
+            position: 'top',
+          },
+          title: {
+            display: true,
+          }
+        }
+    }
+
     return (
         <div>
-            "hello"
+            <div
+            style={
+                {
+                    padding: '20px',
+                    width: '30%'
+                }
+            }>
+                <Pie
+                data={data}
+                options={options}
+                >
+
+                </Pie>
+            </div>
         </div>
     )
 }
+
