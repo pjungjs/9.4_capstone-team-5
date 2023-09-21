af3b3ff2246895708602acf61c11ed1fe85a84ef
pjungjs <pjungjs@gmail.com>
2023-09-21 01:06:02 -0400

    fe_updated ActionDetails.jsx

--- frontend/src/components/Actions/ActionDetails.jsx
+++ frontend/src/components/Actions/ActionDetails.jsx
@@ -1,25 +1,49 @@
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { createClient } from 'contentful';
-import { BLOCKS, MARKS } from '@contentful/rich-text-types';
+import { BLOCKS, MARKS, INLINES } from '@contentful/rich-text-types';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import { TiArrowRightOutline } from 'react-icons/ti';
+import { useStytchSession } from '@stytch/react';
 
 function ActionDetails() {
   const [actionData, setActionData] = useState(null);
+  const navigate = useNavigate();
   const { slug } = useParams();
+
+  const { session } = useStytchSession();
 
   const richTextOptions = {
     renderMark: {
-      [MARKS.BOLD]: (text) => <span className="font-bold">{text}</span>,
+      [MARKS.BOLD]: (text) => (
+        <span className="font-bold text-gray-700">{text}</span>
+      ),
     },
     renderNode: {
-      [BLOCKS.UL_LIST]: (node, children) => <ul>{children}</ul>,
-      [BLOCKS.OL_LIST]: (node, children) => <ol>{children}</ol>,
+      [BLOCKS.HEADING_6]: (node, children) => (
+        <h6 className="py-2 pt-4 text-lg underline">{children}</h6>
+      ),
+      [BLOCKS.UL_LIST]: (node, children) => (
+        <ul className="list-disc">{children}</ul>
+      ),
+      [BLOCKS.OL_LIST]: (node, children) => (
+        <ol className="list-decimal">{children}</ol>
+      ),
       [BLOCKS.LIST_ITEM]: (node, children) => (
-        <li className="list-decimal">{children}</li>
+        <li className="mx-2">{children}</li>
       ),
       [BLOCKS.PARAGRAPH]: (node, children) => (
         <p className="py-1">{children}</p>
+      ),
+      [INLINES.HYPERLINK]: (node, children) => (
+        <a
+          href={node.data.uri}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-blue-400 hover:text-blue-500 hover:underline"
+        >
+          {children}
+        </a>
       ),
     },
   };
@@ -42,8 +66,6 @@ function ActionDetails() {
       );
   }, []);
 
-  console.log(actionData);
-
   return (
     <div className="m-4 mb-12 flex justify-center">
       {actionData && (
@@ -65,7 +87,7 @@ function ActionDetails() {
               alt="Action related image"
               className="max-h-[30rem] object-contain py-2.5"
             />
-            <div className="flex space-x-20 px-4">
+            <div className="flex space-x-20 border-2 border-gray-300 px-4">
               <div>
                 <span className="text-green-600">Points:</span>{' '}
                 {actionData.points}
@@ -122,11 +144,33 @@ function ActionDetails() {
           </div>
           <div className="p-4">
             <div className="text-xl font-bold text-green-600">
-              Track your actions!
+              Track your Actions!
             </div>
-            <div className='py-2'>
-              Create an account or Log in to your account to keep track your actions and points.
-            </div>
+            {session ? (
+              <>
+                <div
+                  onClick={() => navigate('/user/actions')}
+                  className="flex items-center justify-center space-x-2 py-2 text-blue-400 hover:cursor-pointer hover:text-blue-600 hover:underline"
+                >
+                  <span className="text-lg">See Actions on the Dashboard</span>
+                  <TiArrowRightOutline className=" text-2xl" />
+                </div>
+              </>
+            ) : (
+              <>
+                <div className="py-2">
+                  Create or Login to your account to keep track your actions and
+                  accumulate points to earn badges!
+                </div>
+                <div
+                  onClick={() => navigate('/login')}
+                  className="flex items-center justify-center space-x-2 text-blue-400 hover:cursor-pointer hover:text-blue-600 hover:underline"
+                >
+                  <span className="text-xl">Go to Login page</span>
+                  <TiArrowRightOutline className=" text-2xl" />
+                </div>
+              </>
+            )}
           </div>
         </div>
       )}
