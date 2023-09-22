385ff715597179d4a04af78e4740d06c7bcc1cb9
pjungjs <pjungjs@gmail.com>
2023-09-22 02:06:57 -0400

    fe_updated ActionCard styles on Actions page

--- frontend/src/components/Actions/ActionCard.jsx
+++ frontend/src/components/Actions/ActionCard.jsx
@@ -8,17 +8,17 @@ function ActionCard({ content }) {
     <div>
       <div
         onClick={() => navigate(`/actions/${slug}`)}
-        className="items-left m-2 flex min-h-fit w-80 flex-col justify-center rounded-md border-2 border-gray-400 p-4 hover:cursor-pointer hover:bg-green-300"
+        className="items-left m-2 flex w-80 flex-col justify-center rounded-md border-2 border-gray-400 p-4 hover:cursor-pointer hover:bg-green-300"
       >
         <div className="text-center text-lg font-medium">{title}</div>
         <div className="pt-1 text-xs italic">
           Category: <span className="font-medium underline">{category}</span>
         </div>
-        <div>
+        <div className="flex justify-center items-center">
           <img
             src={`https:${relatedImage.fields.file.url}`}
             alt="Action related image"
-            className="object-contain py-2.5"
+            className="object-contain h-60 w-80 py-2.5"
           />
         </div>
         <div className="flex justify-between">
