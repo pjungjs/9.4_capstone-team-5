9339220f7c80d37934a153db3591aa736a4253e7
JoseC620 <Jcepeda6201@gmail.com>
2023-09-27 18:18:46 -0400

    some changes

--- frontend/src/components/User/DailyQuestionsContent/DailyQuestions.jsx
+++ frontend/src/components/User/DailyQuestionsContent/DailyQuestions.jsx
@@ -143,11 +143,16 @@ export default function DailyQuestions( { returningUser }) {
         } else {
         return(
             <div key={index}>
- <button className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-teal-300 to-lime-300 group-hover:from-teal-300 group-hover:to-lime-300 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-lime-800">
-  <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+<button
+  className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-teal-300 to-lime-300 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-lime-800 active:bg-gradient-to-br active:from-lime-300 active:to-lime-300"
+>
+  <span className={`relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 group-focus:bg-opacity-0 group-focus:text-gray-900`}>
     {ans}
   </span>
 </button>
+
+
+
             </div>
         
         ) }
