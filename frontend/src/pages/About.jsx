4c34ea2325580c45dd26472abb2c125417874a84
pjungjs <pjungjs@gmail.com>
2023-10-04 17:45:34 -0400

    fe_fixed typo on About.jsx

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,5 +1,5 @@
-import MainTop from '../components/About/mainTop';
-import AboutTeam from '../components/About/AboutTeam';
+import MainTop from '../components/About/MainTop.jsx';
+import AboutTeam from '../components/About/AboutTeam.jsx';
 import background from '../assets/aboutImages/background.png';
 
 function About() {
