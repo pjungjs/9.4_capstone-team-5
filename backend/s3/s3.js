a9feb5e404e645aeb2369c78bfcdb7b41979439e
pjungjs <pjungjs@gmail.com>
2023-10-06 17:08:04 -0400

    be_updated User's profile picture upload code to AWS S3

--- backend/s3/s3.js
+++ backend/s3/s3.js
@@ -8,27 +8,27 @@ const { v4: uuidv4 } = require('uuid');
 const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
 const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
 const region = process.env.AWS_REGION;
-const bucket = process.env.AWS_S3_BUCKET;
+const Bucket = process.env.AWS_S3_BUCKET;
 
 const s3 = new S3Client({
   credentials: { accessKeyId, secretAccessKey },
   region,
 });
 
-const uploadToS3 = async ({ file, userAuthId }) => {
-  const key = `profilePictures/${userAuthId}/${uuidv4()}`;
-  // const key = `${userAuthId}/${new Date().toISOString()}`;
+const usersPictureToS3 = async ({ file, userAuthId }) => {
+  const Key = `profilePictures/${userAuthId}/${uuidv4()}`;
+  // const Key = `${userAuthId}/${new Date().toISOString()}`;
 
   const command = new PutObjectCommand({
-    Bucket: bucket,
-    Key: key,
+    Bucket,
+    Key,
     Body: file.buffer,
     ContentType: file.mimetype,
   });
 
   try {
     await s3.send(command);
-    return { success: true, payload: key };
+    return { success: true, payload: Key };
   } catch (error) {
     console.log(error);
     return {
@@ -41,7 +41,7 @@ const uploadToS3 = async ({ file, userAuthId }) => {
 const getUserImagesInfo = async (userAuthId) => {
   try {
     const command = new ListObjectsV2Command({
-      Bucket: bucket,
+      Bucket,
       Prefix: `profilePictures/${userAuthId}`,
     });
 
@@ -59,6 +59,6 @@ const getUserImagesInfo = async (userAuthId) => {
 };
 
 module.exports = {
-  uploadToS3,
+  usersPictureToS3,
   getUserImagesInfo,
 };
