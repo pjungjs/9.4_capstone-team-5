9c945fb73f4db827cf3b2c5a56415a47c81b2413
pjungjs <pjungjs@gmail.com>
2023-10-08 02:54:06 -0400

    fe_fixed syntax error on User's Leaderboard page. refactored code

--- frontend/src/components/User/LeaderboardContent/LeaderboardDisplay.jsx
+++ frontend/src/components/User/LeaderboardContent/LeaderboardDisplay.jsx
@@ -2,14 +2,13 @@ import Profiles from '../LeaderboardContent/Profiles.jsx';
 
 function LeaderboardDisplay() {
   return (
-
-    <div className='cust-bg-background'> {/* ask jinseok why when i put the leaderboard here */}
-     <div>
-        <p className="text-5xl font-semibold mb-4 text-center font-bungee pt-10 ">Leaderboard</p>  
-
-        </div>
-      <Profiles />  
-
+    <div className="cust-bg-background">
+      <div>
+        <p className="mb-4 pt-10 text-center font-bungee text-5xl font-semibold">
+          Leaderboard
+        </p>
+      </div>
+      <Profiles />
     </div>
   );
 }
