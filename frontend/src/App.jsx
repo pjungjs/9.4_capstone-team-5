e9a59c9af902b288cc0988c7e9be32f561a00397
pjungjs <pjungjs@gmail.com>
2023-10-07 21:27:39 -0400

    fe_added Post Edit functionality in Community Forum Post Detail page. refactored files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,17 +6,16 @@ import Footer from './components/common/Footer.jsx';
 import Home from './pages/Home.jsx';
 import About from './pages/About.jsx';
 import Contact from './pages/Contact.jsx';
-import NotFound from './pages/NotFound.jsx';
 import Login from './pages/Login.jsx';
 import LoginAuth from './components/Login/LoginAuth.jsx';
 import PrivateRoutes from './components/User/PrivateRoutes.jsx';
 import UserMain from './components/User/UserMain.jsx';
-import Forum from './pages/ForumTopic.jsx';
-import ForumTopic from './pages/Forum.jsx';
-import CommunityForum from './pages/CommunityForum.jsx';
-import PostDetails from './components/Posts/PostDetails.jsx';
 import Actions from './components/Actions/Actions.jsx';
 import ActionDetails from './components/Actions/ActionDetails.jsx';
+import CommunityForum from './pages/CommunityForum.jsx';
+import PostDetails from './components/Posts/PostDetails.jsx';
+import EditPost from './components/Posts/EditPost.jsx';
+import NotFound from './pages/NotFound.jsx';
 
 function App() {
   return (
@@ -31,15 +30,20 @@ function App() {
             <Route path="/contact" element={<Contact />} />
             <Route path="/login" element={<Login />} />
             <Route path="/login/auth" element={<LoginAuth />} />
-            <Route path="/forum" element={<Forum />} />
-            <Route path="/topic/:id" element={<ForumTopic />} />
-            <Route path="/actions" element={<Actions />} />
-            <Route path="/actions/:slug" element={<ActionDetails />} />
-            <Route path="/community-forum" element={<CommunityForum />} />
-            <Route path="/community-forum/post/:slug" element={<PostDetails />} />
             <Route element={<PrivateRoutes />}>
               <Route path="/user/*" element={<UserMain />} />
             </Route>
+            <Route path="/actions" element={<Actions />} />
+            <Route path="/actions/:slug" element={<ActionDetails />} />
+            <Route path="/community-forum" element={<CommunityForum />} />
+            <Route
+              path="/community-forum/post/:slug"
+              element={<PostDetails />}
+            />
+            <Route
+              path="/community-forum/post/edit/:slug"
+              element={<EditPost />}
+            />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </main>
