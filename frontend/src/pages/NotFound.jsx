5a810214cf852efa83ac8cb8f3d9a57fec784847
shareekaepps <shareekaepps@icloud.com>
2023-10-08 14:37:53 -0400

    not-found page css

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -2,18 +2,18 @@ import { Link } from 'react-router-dom';
 
 function NotFound() {
   return (
-    <main className="flex h-screen w-full flex-col items-center justify-center bg-green-400">
+    <main className="flex h-screen w-full flex-col items-center justify-center bg-[#00b2eede]">
       <h1 className="text-9xl font-extrabold tracking-widest text-white">
         404
       </h1>
-      <div className="absolute rotate-12 rounded bg-[#1A2238] px-2 text-sm text-[#fc5c2b]">
+      <div className="text-md absolute rotate-12 rounded bg-[#ff0000] px-2">
         Page Not Found!
       </div>
       <button className="mt-5">
-        <div className="focus:ooutline-none group relative inline-block text-sm font-medium text-[#fc5c2b] focus:ring active:text-red-500">
-          <span className="absolute inset-0 translate-x-0.5 translate-y-0.5 bg-[#fc5c2b] transition-transform group-hover:translate-x-0 group-hover:translate-y-0"></span>
+        <div className="group relative inline-block text-sm font-medium text-green-400 focus:outline-none focus:ring active:text-red-500">
+          <span className="absolute inset-0 translate-x-0.5 translate-y-0.5 text-green-400 transition-transform group-hover:translate-x-0 group-hover:translate-y-0"></span>
 
-          <span className="relative block border border-current bg-[#1A2238] px-8 py-3">
+          <span className="text-md relative block border border-current bg-[#1A2238] px-8 py-3">
             <Link to="/">Go Home</Link>
           </span>
         </div>
